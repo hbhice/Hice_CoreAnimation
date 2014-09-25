8a9604d7e1b84cc506f8e3574714cf46d1a5bb4a
HuangBing <bhuang@HuangBings-MacBook-Pro.local>
2014-09-25 16:17:02 +0800

    Initial Commit

---
+++ 3DFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DFun
+//
+//  Created by HuangBing on 9/25/14.
+//  Copyright (c) 2014 HuangBing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 3DFun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  3DFun
+//
+//  Created by HuangBing on 9/25/14.
+//  Copyright (c) 2014 HuangBing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
